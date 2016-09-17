fa8b4bd269c463e5ce5bfad79782c55ef22404df
unknown <priyanka@fierydevs.com>
2016-09-17 15:01:00 +0530

    search place and mark it on map,move marker on map and fetch address

--- app/src/main/java/com/fierydevs/googlemaps/activity/MainActivity.java
+++ app/src/main/java/com/fierydevs/googlemaps/activity/MainActivity.java
@@ -1,22 +1,33 @@
 package com.fierydevs.googlemaps.activity;
 
+import android.app.Activity;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.fierydevs.googlemaps.R;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.places.AutocompleteFilter;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.places.ui.PlaceAutocompleteFragment;
+import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -45,19 +56,101 @@ public class MainActivity extends AppCompatActivity
     Marker mCurrLocationMarker;
 
 
+    private static String TAG = MainActivity.class.getSimpleName();
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        showAddress = (TextView) findViewById(R.id.showAddress);
+        //showAddress = (TextView) findViewById(R.id.showAddress);
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkLocationPermission();
         }
+        int off = 0;
+        try {
+            off = Settings.Secure.getInt(getContentResolver(), Settings.Secure.LOCATION_MODE);
+        } catch (Settings.SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(off==0){
+            displayPromptForEnablingGPS(this);
+        }
+        PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
+                getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
+        AutocompleteFilter typeFilter = new AutocompleteFilter.Builder()
+                .setTypeFilter(AutocompleteFilter.TYPE_FILTER_ADDRESS)
+                .build();
+        autocompleteFragment.setFilter(typeFilter);
+
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+                Log.i(TAG, "Place: " + place.getName());
+                LatLng location = place.getLatLng();
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(location);
+                Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+                List<android.location.Address> addresses;
+                String s = "";
+                try {
+                    addresses = gcd.getFromLocation(location.latitude,location.longitude,1);
+                    if (addresses.size() > 0)
+                        s = addresses.get(0).getAddressLine(0) + "\t"+
+                                addresses.get(0).getSubLocality() + "\t" +
+                                addresses.get(0).getLocality() + "\t" +
+                                addresses.get(0).getPostalCode() + "\t" +
+                                addresses.get(0).getAdminArea() + "\t" +
+                                addresses.get(0).getCountryCode();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                markerOptions.title(s);
+                mGoogleMap.clear();
+                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLng(location));
+                mGoogleMap.addMarker(markerOptions);
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        });
 
         mapFrag = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFrag.getMapAsync(this);
+
+    }
+
+    public static void displayPromptForEnablingGPS(
+            final Activity activity)
+    {
+        final AlertDialog.Builder builder =
+                new AlertDialog.Builder(activity);
+        final String action = Settings.ACTION_LOCATION_SOURCE_SETTINGS;
+        final String message = "Enable either GPS or any other location"
+                + " service to find current location.  Click OK to go to"
+                + " location services settings to let you do so.";
+
+        builder.setMessage(message)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface d, int id) {
+                                activity.startActivity(new Intent(action));
+                                d.dismiss();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface d, int id) {
+                                d.cancel();
+                            }
+                        });
+        builder.create().show();
     }
 
     @Override
@@ -73,22 +166,39 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onMapReady(GoogleMap googleMap)
     {
-        mGoogleMap=googleMap;
-        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mGoogleMap = googleMap;
 
-        //Initialize Google Play Services
-        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (ContextCompat.checkSelfPermission(this,
-                    android.Manifest.permission.ACCESS_FINE_LOCATION)
-                    == PackageManager.PERMISSION_GRANTED) {
-                buildGoogleApiClient();
-                mGoogleMap.setMyLocationEnabled(true);
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mGoogleMap.setMyLocationEnabled(true);
+        buildGoogleApiClient();
+        mGoogleMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(LatLng latLng) {
+                Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
+                List<android.location.Address> addresses;
+                String s = "";
+                try {
+                    addresses = gcd.getFromLocation(latLng.latitude,latLng.longitude,1);
+                    if (addresses.size() > 0)
+                        s = addresses.get(0).getAddressLine(0) + "\t"+
+                            addresses.get(0).getSubLocality() + "\t" +
+                            addresses.get(0).getLocality() + "\t" +
+                            addresses.get(0).getPostalCode() + "\t" +
+                            addresses.get(0).getAdminArea() + "\t" +
+                            addresses.get(0).getCountryCode();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title(s);
+                mGoogleMap.clear();
+                mGoogleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                mGoogleMap.addMarker(markerOptions);
             }
-        }
-        else {
-            buildGoogleApiClient();
-            mGoogleMap.setMyLocationEnabled(true);
-        }
+        });
     }
 
     protected synchronized void buildGoogleApiClient() {
@@ -130,34 +240,9 @@ public class MainActivity extends AppCompatActivity
         //Place current location marker
         LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
         MarkerOptions markerOptions = new MarkerOptions();
-        String s = null;
-        Geocoder gcd = new Geocoder(getBaseContext(), Locale.getDefault());
-        List<android.location.Address> addresses;
-        try {
-            addresses = gcd.getFromLocation(location.getLatitude(),
-                    location.getLongitude(), 1);
-            if (addresses.size() > 0)
-            s =     "Country name: "
-                    + addresses.get(0).getCountryName()
-                    + "\n" + "Country code: "
-                    + addresses.get(0).getCountryCode()
-                    + "\n" + "State: "
-                    + addresses.get(0).getAdminArea()
-                    + "\n" + "Locality: "
-                    + addresses.get(0).getLocality()
-                    + "\n" + "SubLocality: "
-                    + addresses.get(0).getSubLocality()
-                    + "\n" + "Address Line: "
-                    + addresses.get(0).getAddressLine(0)
-                    + "\n" + "Postbox: "
-                    + addresses.get(0).getPostalCode();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        showAddress.setText(String.valueOf(latLng)+"\n" + s);
         markerOptions.position(latLng);
         markerOptions.title("Current Location");
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_MAGENTA));
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED));
         mCurrLocationMarker = mGoogleMap.addMarker(markerOptions);
 
         //move map camera
